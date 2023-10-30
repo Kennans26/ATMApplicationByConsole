3ae6952fbbfd30e0c8117f7eb6d3c0696d0dd5b8
Kennans26 <info.kennans26@gmail.com>
2023-10-30 22:32:13 +0400

    Domain/Entities - AccountUser class was added! There are 3 default user will be. It will help to understand how our codes works.

--- ATMApp/ATMApp/App/ATMApp.cs
+++ ATMApp/ATMApp/App/ATMApp.cs
@@ -1,10 +1,50 @@
-﻿using ATMApp.UI;
+﻿using ATMApp.Domain.Entities;
+using ATMApp.UI;
 using System;
 
 namespace ATMApp.App
 {
     internal class ATMApp
     {
+        private List<UserAccount> userAccountList;
+        private UserAccount selectedAccount;
+
+        public void InitializedData()
+        {
+            userAccountList = new List<UserAccount>
+            {
+                new UserAccount
+                {
+                    Id = 1,
+                    FullName = "FullName1",
+                    AccountNumber = 001,
+                    CardNumber = 123456,
+                    CardPin = 123123,
+                    AccountBalance = 50000.00m,
+                    IsLocked = false
+                },
+                new UserAccount
+                {
+                    Id = 2,
+                    FullName = "FullName2",
+                    AccountNumber = 002,
+                    CardNumber = 654321,
+                    CardPin = 321321,
+                    AccountBalance = 4000.00m,
+                    IsLocked = false
+                },
+                new UserAccount
+                {
+                    Id = 3,
+                    FullName = "FullName3",
+                    AccountNumber = 003,
+                    CardNumber = 135790,
+                    CardPin = 024680,
+                    AccountBalance = 2000.00m,
+                    IsLocked = true
+                }
+            };
+        }
         private static void Main(string[] args)
         {
             AppScreen.Welcome();
